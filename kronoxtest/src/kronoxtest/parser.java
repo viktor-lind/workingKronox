14172beff8d8b0b21bd2f91bc2e9e28ce7335c00
viktor-lind <viktor_781@Hotmail.com>
2016-03-17 22:40:37 +0100

    Complete code

---
+++ kronoxtest/src/kronoxtest/parser.java
@@ -0,0 +1,28 @@
+package kronoxtest;
+
+public class parser {
+
+		private String lokal;
+		private String kurs;
+		private String endTime;
+		private String startTime;
+		
+
+		public parser(String lokal, String kurs, String startTime, String endTime) {
+			this.lokal = lokal;
+			this.kurs = kurs;
+			this.endTime = endTime;
+			this.startTime = startTime;
+			
+		}
+
+
+		public String toString(){
+			String string = startTime + " - " + endTime + " " + lokal + " "  + kurs;
+			return string;
+		}
+		
+
+	}
+
+
